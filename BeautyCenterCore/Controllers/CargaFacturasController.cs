cd34b9a70a7602544d02ee4f1f3a6137e8950735
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-07-25 12:07:43 -0400

    aqui

---
+++ BeautyCenterCore/Controllers/CargaFacturasController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BeautyCenterCore.Models;
+using BeautyCenterCore.BLL;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BeautyCenterCore.Controllers
+{
+    public class CargaFacturasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult ServiciosFacturas(int id)
+        {
+            var listado = BLL.ServiciosBLL.ListarId(id);
+
+            return Json(listado);
+        }
+        [HttpGet]
+        public JsonResult ListaServiciosFacturas(int id)
+        {
+            var listado = BLL.ServiciosBLL.Listar();
+
+            return Json(listado);
+        }
+        [HttpGet]
+        public JsonResult ListaClientesFacturas(int id)
+        {
+            var listado = BLL.ClientesBLL.Listar();
+
+            return Json(listado);
+        }
+        [HttpGet]
+        public JsonResult ListaEmpleadosFacturas(int id)
+        {
+            var listado = BLL.EmpleadosBLL.Listar();
+
+            return Json(listado);
+        }
+        [HttpPost]
+        public JsonResult GuardarFacturas(Facturas nueva)
+        {
+            int id = 0;
+            if (ModelState.IsValid)
+            {
+                if (FacturasBLL.Guardar(nueva))
+                {
+                    id = nueva.FacturaId;
+                }
+            }
+            return Json(id);
+        }
+        [HttpPost]
+        public JsonResult GuardarDetalleFacturas([FromBody]List<FacturaDetalles> detalles)
+        {
+            bool resultado = BLL.FacturaDetallesBLL.Insertar(detalles);
+
+            return Json(resultado);
+        }
+        
+    }
+}
